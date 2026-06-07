a78865a7b9ffa12c2cb032752af25cf818583d21
haoxinlei <howxl97@163.com>
2026-06-07 16:29:28 +0800

    fix: 恢复误删的深度学习文件

---
+++ 文章/LLM/深度学习/001.py
@@ -0,0 +1,8 @@
+'''
+Author: haoxinlei howxl97@163.com
+Date: 2026-05-01 12:58:12
+LastEditors: haoxinlei howxl97@163.com
+LastEditTime: 2026-05-01 13:26:24
+FilePath: /notes/LLM/深度学习/001.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
